0a5fce53c4be55ee2999d0176b48c1cc1af94eaf
showmark <1017033681@qq.com>
2016-09-07 23:17:10 +0800

    delete MatchProjet

--- MatchProject/gen/team/yjcollege/matchproject/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package team.yjcollege.matchproject;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- MatchProject/src/team/yjcollege/matchproject/ui/FragmentInterface.java
+++
@@ -1,9 +0,0 @@
-package team.yjcollege.matchproject.ui;
-
-/**
- * FragmentInterface of Health871
- */
-public interface FragmentInterface {
-    public void refreshViews();
-    public boolean commitData();
-}
